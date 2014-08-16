ed77f87575abba2bf355c04b39f7febad4c1c303
Johannes-Maria Frank <jmfrank63@gmail.com>
2014-08-17 01:35:52 +0200

    added initial code
    Changed names of variables from vector example and replaced mean
    library function with solve library function

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        mat <<- y
+        inv <<- NULL
+    }
+    get <- function() mat
+    set.inverse <- function(solve) inv <<- solve
+    get.inverse <- function() inv
+    list(set = set, get = get,
+         set.inverse = set.inverse,
+         get.inverse = get.inverse)
+    mat
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$get.inverse()
+    if(!is.null(inv)) {
+        message("getting cached inverse")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$set.inverse(inv)
+    inv
 }
