5bb66182191ef8d5a76043030d0199cdf318f56a
ItsJaneeeey <67644863+ItsJaneeeey@users.noreply.github.com>
2021-01-14 18:33:25 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,54 +1,23 @@
-## cached matrix inversion functions for 
-## programming assignment 2
-## demonstrates lexical scoping
-
-## creates a matrix with getters and setters
-## so regetting of an inverse matrix gets
-## result form cache instead of recalculating
-
-makeCacheMatrix <- function(mat = matrix()) {
-    # initialize the inverse to NULL
+makeCacheMatrix <- function(x = matrix()){
     inv <- NULL
-    
-    # setter for matrix
-    set <- function(mtrx) {
-        mat <<- mtrx
-        inv <<- NULL
+    set <- function(y){
+      x<<- y
+      inv<<- NULL
     }
-    
-    # getter for matrix
-    get <- function() mat
-    
-    # setter for the inverse
-    set.inverse <- function(setinv) inv <<- setinv
-    
-    # getter for the inverse
-    get.inverse <- function() inv
-    
-    # returns a list of getter and setter functions
-    list(set = set, get = get,
-         set.inverse = set.inverse,
-         get.inverse = get.inverse)
+    get<- function() {x}
+    setInverse <- function(inverse) {inv <<- inverse}
+    getInverse <- function() {inv}
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## cache solves the inverse of a matrix
-## if already calculated returns cached
-## results instead
-
-cacheSolve <- function(cached.mat, ...) {
-    # get the stored inverse
-    inv <- cached.mat$get.inverse()
-    # if it exists return it
-    if(!is.null(inv)) {
-        message("getting cached inverse")
-        return(inv)
-    }
-    # else calculate, store and return it
-    raw.mat <- cached.mat$get()
-    inv <- solve(raw.mat, ...)
-    cached.mat$set.inverse(inv)
-    
-    # returns the inverse
-    inv
+cacheSolve <- function(x, ...){
+   inv <- x$getInverse()
+    if(!is.null(inv)){
+      message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
